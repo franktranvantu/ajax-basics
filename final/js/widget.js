9f9118e765882f2f38e161d9e764f7afb8dcb4dc
Frank Tran <franktranvantu@gmail.com>
2021-02-04 22:36:03 +0700

    Update

--- final/js/widget.js
+++ final/js/widget.js
@@ -1,7 +1,7 @@
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
-  if(xhr.readyState === 4 && xhr.status === 200) {
-    var employees = JSON.parse(xhr.responseText);
+var employeeXhr = new XMLHttpRequest();
+employeeXhr.onreadystatechange = function () {
+  if(employeeXhr.readyState === 4 && employeeXhr.status === 200) {
+    var employees = JSON.parse(employeeXhr.responseText);
     var statusHTML = '<ul class="bulleted">';
     for (var i=0; i<employees.length; i += 1) {
       if (employees[i].inoffice === true) {
@@ -16,5 +16,26 @@ xhr.onreadystatechange = function () {
     document.getElementById('employeeList').innerHTML = statusHTML;
   }
 };
-xhr.open('GET', '../data/employees.json');
-xhr.send();
+employeeXhr.open('GET', 'data/employees.json');
+employeeXhr.send();
+
+var roomXhr = new XMLHttpRequest();
+roomXhr.onreadystatechange = function () {
+  if(roomXhr.readyState === 4 && roomXhr.status === 200) {
+    var rooms = JSON.parse(roomXhr.responseText);
+    var statusHTML = '<ul class="rooms">';
+    for (var i=0; i<rooms.length; i += 1) {
+      if (rooms[i].available === true) {
+        statusHTML += '<li class="empty">';
+      } else {
+        statusHTML += '<li class="full">';
+      }
+      statusHTML += rooms[i].room;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('roomList').innerHTML = statusHTML;
+  }
+};
+roomXhr.open('GET', 'data/rooms.json');
+roomXhr.send();
